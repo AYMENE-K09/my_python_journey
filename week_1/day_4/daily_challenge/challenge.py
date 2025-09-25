a716e15e7499da7d401b6858402178740d7f1443
AK <khalidaymene09@gmail.com>
2025-09-25 19:24:34 +0100

    EXERCISES

--- week_1/day_4/daily_challenge/challenge.py
+++ week_1/day_4/daily_challenge/challenge.py
@@ -1,67 +1,44 @@
 import math
+
 class Pagination():
-    def __init__(self, items, page_size = 10):
+    def __init__(self, items = [], page_size = 10):
+        self.page_size = page_size
         self.items = items
-        self.page_size = page_size # items displayed by page
-        if self.items is None:
-            self.items = list(items)
         self.current_idx = 0
-        self.Total_N_of_pages = math.ceil(len(self.items) / self.page_size)
+        self.Total_n_of_pages = math.ceil(len(self.items) / page_size)
+
 
     def get_visible_items(self):
-            self.start_index = (self.current_idx - 1) * self.page_size
-            self.end_index = min((self.start_index + self.page_size), len(self.items))
-            for i in range(self.start_index, self.end_index):
-                print(self.items[i])
+        if self.current_idx == 0:
+            self.current_idx += 1
+            start_idx = (self.current_idx - 1) * self.page_size
+            end_idx = min(start_idx + self.page_size, len(self.items))
+            my_current_page = []
+            for i in range(start_idx, end_idx):
+                my_current_page.append(self.items[i])
+            return my_current_page
 
+    def go_to_page(self, page_num):
+        if page_num == 0:
+            raise ValueError("Page not found")
+        
+        self.current_idx = page_num
 
-    def go_to_page(self ,page_num):
-        self.page_num = page_num
-        if  0 >= page_num or page_num > self.Total_N_of_pages:
-            print("page not found")
-
-        else:
-            self.current_idx = page_num
-            self.start_index = (self.current_idx - 1) * self.page_size
-            self.end_index = min((self.start_index + self.page_size), len(self.items))
-            for i in range(self.start_index, self.end_index):
-                print(self.items[i])
-            
-            
     def first_page(self):
-        self.start_index = (1 - 1) * self.page_size
-        self.end_index = min((self.start_index + self.page_size), len(self.items))
-        for i in range(self.start_index, self.end_index):
-            print(self.items[i])
+        self.current_idx = 1
 
     def last_page(self):
-        self.start_index = (self.Total_N_of_pages - 1) * self.page_size
-        self.end_index = min((self.start_index + self.page_size), len(self.items))
-        for i in range(self.start_index, self.end_index):
-            print(self.items[i])
+        self.current_idx = self.Total_n_of_pages
 
     def next_page(self):
-        if self.current_idx < self.Total_N_of_pages:
+        if self.current_idx != self.Total_n_of_pages:
             self.current_idx += 1
-            self.start_index = (self.current_idx - 1) * self.page_size
-            self.end_index = min((self.start_index + self.page_size), len(self.items))
-            for i in range(self.start_index, self.end_index):
-                print(self.items[i])
-        
-        else:
-            print("page not found")
 
     def previous_page(self):
-        if self.current_idx > 0:
+        if self.current_idx != 1:
             self.current_idx -= 1
-            self.start_index = (self.current_idx - 1) * self.page_size
-            self.end_index = min((self.start_index + self.page_size), len(self.items))
-            for i in range(self.start_index, self.end_index):
-                print(self.items[i])
-        
-        else:
-            print("page not found")
 
-
-my_list = Pagination("abcdefghijklmnopqrstuvwyxz", 4)
-my_list.get_visible_items()
+    # def__str__
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+p = Pagination(alphabet, 4)
+print(p.get_visible_items())
